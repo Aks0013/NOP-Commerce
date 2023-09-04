a9431b78ce32e90e24fbeadfb32200f33e48e7dc
Aks0013 <142399814+Aks0013@users.noreply.github.com>
2023-09-04 06:53:37 +0000

    Add files via upload

---
+++ CurrencySelect.java
@@ -0,0 +1,19 @@
+package Gokul;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class CurrencySelect {
+	WebDriver driver;
+	public CurrencySelect(WebDriver driver)
+	{
+		this.driver=driver;
+	}
+	
+	By CurrencySelector=By.xpath("//select[@id='customerCurrency']");
+	public WebElement Currency()
+	{
+		return driver.findElement(CurrencySelector);
+	}	
+}
